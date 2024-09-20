62a8242dce671d2beef51ab182e87e4f1b11ac6c
macTan <tanhaosmail@gmail.com>
2024-09-20 14:00:40 -0400

    feat: Added Major components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
+import useStyles from './styles';
+import useAlan from './Alan';
+
+import { Movies, Actors, MovieInfo, Navbar, Profile } from './index';
+
+function App() {
+  const classes = useStyles();
+  const alanBtnContainer = useRef();
+
+  useAlan();
+
   return (
-    <div>
-        App
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/approved" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInfo />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+      <div ref={alanBtnContainer} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
